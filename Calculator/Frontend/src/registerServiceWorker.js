0af37c7d74c016be2a6eda27017709a121c69bb1
Akshay <akshay.bajaj@sjsu.edu>
2019-09-16 00:48:22 -0700

    Updating code by changing local host address to AWS EC2 address

--- Calculator/Frontend/src/registerServiceWorker.js
+++ Calculator/Frontend/src/registerServiceWorker.js
@@ -8,11 +8,11 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
-const isec2-18-191-137-127.us-east-2.compute.amazonaws.com = Boolean(
-  window.location.hostname === 'ec2-18-191-137-127.us-east-2.compute.amazonaws.com' ||
-    // [::1] is the IPv6 ec2-18-191-137-127.us-east-2.compute.amazonaws.com address.
+const islocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered ec2-18-191-137-127.us-east-2.compute.amazonaws.com for IPv4.
+    // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
@@ -32,11 +32,11 @@ export default function register() {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (!isec2-18-191-137-127.us-east-2.compute.amazonaws.com) {
+      if (!islocalhost) {
         // Is not local host. Just register service worker
         registerValidSW(swUrl);
       } else {
-        // This is running on ec2-18-191-137-127.us-east-2.compute.amazonaws.com. Lets check if a service worker still exists or not.
+        // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
       }
     });
